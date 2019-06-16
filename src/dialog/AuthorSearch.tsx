bdb32798a8e47c7160dee76918ae59c49c81842e
Grigorii Liullin <grigorii.liullin@t-systems.com>
2019-06-16 17:21:24 +0200

    Implement autocomplete for genres and authors using webflux backend

---
+++ src/dialog/AuthorSearch.tsx
@@ -0,0 +1,104 @@
+import * as React from "react";
+import {AsyncTypeahead} from 'react-bootstrap-typeahead';
+
+const url = process.env.REACT_APP_URL;
+const searchUrl = process.env.REACT_APP_SEARCH_URL;
+
+type AuthorSearchProps = {
+    handleChangeAuthorLinks: { (authorLinks: Array<string>): void }
+    clean: boolean
+}
+
+type AuthorSearchState = {
+    Author: string,
+    options: [{ author: string, self: string }],
+    selected: { author: string, self: string }[],
+    isLoading: boolean
+    clean: boolean
+}
+
+export class AuthorSearch extends React.Component<AuthorSearchProps, AuthorSearchState> {
+
+    private typeahead: any;
+
+    constructor(props: AuthorSearchProps) {
+        super(props);
+        this.state = {
+            Author: '',
+            options: [{author: '', self: ''}],
+            selected: [{author: '', self: ''}],
+            isLoading: false,
+            clean: props.clean
+        };
+
+        this.handleSearchAuthor = this.handleSearchAuthor.bind(this);
+        this.handleChangeAuthor = this.handleChangeAuthor.bind(this);
+    }
+
+    loadAuthors(query: string) {
+        console.log('+AuthorSearch.loadAuthors');
+        fetch(searchUrl + 'authors/searchByName?name=' + query)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                let authors = result.map((a: any) => {
+                    return {self: url + 'authors/' + a.id, author: a.fullName};
+                });
+                this.setState({
+                    isLoading: false,
+                    options: authors
+                });
+            }, error => {
+                console.error(error)
+            });
+    }
+
+    handleSearchAuthor(query: string) {
+        console.log('+CreateBookDialog.handleSearchAuthor');
+        this.setState({isLoading: true});
+        this.loadAuthors(query);
+    }
+
+    handleChangeAuthor(selected: { author: string, self: string }[]) {
+        console.log('+CreateBookDialog.handleChangeAuthor');
+        this.setState({selected: selected});
+        const links = selected.map((a: any) => a.self);
+        this.props.handleChangeAuthorLinks(links);
+    }
+
+    componentWillReceiveProps(nextProps: Readonly<AuthorSearchProps>, nextContext: any): void {
+        console.log('+AuthorSearch.componentWillReceiveProps', nextProps);
+        if (nextProps.clean !== this.props.clean) {
+            this.setState({
+                clean: nextProps.clean
+            });
+        }
+    }
+
+    render() {
+        const clean = this.state.clean;
+        if (clean && this.typeahead) {
+            this.typeahead._instance.clear();
+            this.setState({
+                clean: !clean
+            });
+        }
+        return (
+            <div className='form-group'>
+                <label htmlFor='Author'>Author</label>
+                <AsyncTypeahead
+                    id='AuthorInput'
+                    multiple
+                    minLength={1}
+                    labelKey='author'
+                    placeholder="Start to type a Author..."
+                    isLoading={this.state.isLoading}
+                    onSearch={this.handleSearchAuthor}
+                    onChange={this.handleChangeAuthor}
+                    options={this.state.options}
+                    ref={(ref) => this.typeahead = ref}
+                />
+            </div>
+        );
+    }
+}
---
+++ src/dialog/GenreSearch.tsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import {AsyncTypeahead} from 'react-bootstrap-typeahead';
+
+const url = process.env.REACT_APP_URL;
+const searchUrl = process.env.REACT_APP_SEARCH_URL;
+
+type GenreSearchProps = {
+    handleChangeGenreLinks: { (genreLinks: Array<string>): void },
+    clean: boolean
+}
+
+type GenreSearchState = {
+    genre: string,
+    options: [{ genre: string, self: string }],
+    selected: { genre: string, self: string }[],
+    isLoading: boolean,
+    clean: boolean
+}
+
+export class GenreSearch extends React.Component<GenreSearchProps, GenreSearchState> {
+
+    private typeahead: any;
+
+    constructor(props: GenreSearchProps) {
+        super(props);
+        this.state = {
+            genre: '',
+            options: [{genre: '', self: ''}],
+            selected: [{genre: '', self: ''}],
+            isLoading: false,
+            clean: props.clean
+        };
+
+        this.handleSearchGenre = this.handleSearchGenre.bind(this);
+        this.handleChangeGenre = this.handleChangeGenre.bind(this);
+    }
+
+    loadGenres(query: string) {
+        console.log('+GenreSearch.loadGenres');
+        fetch(searchUrl + 'genres/searchByGenre?genre=' + query)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                let genres = result.map((g: any) => {
+                    return {self: url + 'genres/' + g.id, genre: g.genre};
+                });
+                this.setState({
+                    isLoading: false,
+                    options: genres
+                });
+            }, error => {
+                console.error(error)
+            });
+    }
+
+    handleSearchGenre(query: string) {
+        console.log('+CreateBookDialog.handleSearchGenre');
+        this.setState({isLoading: true});
+        this.loadGenres(query);
+    }
+
+    handleChangeGenre(selected: { genre: string, self: string }[]) {
+        console.log('+CreateBookDialog.handleChangeGenre');
+        this.setState({selected: selected});
+        const links = selected.map((g: any) => g.self);
+        this.props.handleChangeGenreLinks(links);
+    }
+
+    componentWillReceiveProps(nextProps: Readonly<GenreSearchProps>, nextContext: any): void {
+        console.log('+AuthorSearch.componentWillReceiveProps', nextProps);
+        if (nextProps.clean !== this.props.clean) {
+            this.setState({
+                clean: nextProps.clean
+            });
+        }
+    }
+
+    render() {
+        const clean = this.state.clean;
+        if (clean && this.typeahead) {
+            this.typeahead._instance.clear();
+            this.setState({
+                clean: !clean
+            });
+        }
+        return (
+            <div className='form-group'>
+                <label htmlFor='genre'>Genre</label>
+                <AsyncTypeahead
+                    id='genreInput'
+                    multiple
+                    minLength={1}
+                    labelKey='genre'
+                    placeholder="Start to type a genre..."
+                    isLoading={this.state.isLoading}
+                    onSearch={this.handleSearchGenre}
+                    onChange={this.handleChangeGenre}
+                    options={this.state.options}
+                    ref={(ref) => this.typeahead = ref}
+                />
+            </div>
+
+        );
+    }
+}
